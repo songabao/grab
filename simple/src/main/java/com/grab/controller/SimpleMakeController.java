b0e75ee04862ab0d79511e34af11eed8108fd942
songabao <1919103133@qq.com>
2019-09-05 13:38:00 +0800

    通过url抓取网页数据

---
+++ simple/src/main/java/com/grab/controller/SimpleMakeController.java
@@ -0,0 +1,15 @@
+package com.grab.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author songabao
+ * @date 2019/9/5 13:36
+ */
+@RestController
+public class SimpleMakeController {
+    private static  final Logger log = LoggerFactory.getLogger(SimpleMakeController.class);
+
+}
